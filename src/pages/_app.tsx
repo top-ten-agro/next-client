8a290d1e91c77bec1267a89243bf482b66bffb2f
Touha Zohair <touha98@gmail.com>
2023-04-24 13:30:13 +0600

    tailwind -> mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider, createTheme } from "@mui/material";
 import { DefaultLayout } from "@/layouts/DefaultLayout";
 
 import "@/styles/globals.css";
 
 const queryClient = new QueryClient();
+const theme = createTheme({});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -15,9 +17,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session} refetchInterval={300}>
       <QueryClientProvider client={queryClient}>
-        <DefaultLayout>
-          <Component {...pageProps} />
-        </DefaultLayout>
+        <ThemeProvider theme={theme}>
+          <DefaultLayout>
+            <Component {...pageProps} />
+          </DefaultLayout>
+        </ThemeProvider>
       </QueryClientProvider>
     </SessionProvider>
   );
